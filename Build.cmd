d2f94d758020a9c9617bc91f8935c8988ed1fd0c
Tobias Koch <t.koch@tk-software.de>
2018-03-29 17:45:01 +0200

    Updating build script

--- Build.cmd
+++ Build.cmd
@@ -7,11 +7,21 @@ echo Restoring nuget packages
 nuget restore LinkTime.sln
 if errorlevel 1 goto error
 
+echo Setting version number
+Build\Packages\Veronique.1.2.0\tools\Veronique
+if errorlevel 1 goto error
+
 echo Building solution (release)
 msbuild.exe /consoleloggerparameters:ErrorsOnly /maxcpucount /nologo ^
   /property:Configuration=Release /property:Platform="Any CPU" ^
   /verbosity:quiet ^
   LinkTime.sln
+if errorlevel 1 goto error
+
+echo Cleaning up
+git checkout -- LinkTime/LinkTime/Properties/AssemblyInfo.cs
+if errorlevel 1 goto error
+git checkout -- LinkTime/LinkTime.Test/Properties/AssemblyInfo.cs
 if errorlevel 1 goto error
 
 :success
